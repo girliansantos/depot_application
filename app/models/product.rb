56030f64fff4f95fb8e02422621fd9398b2a1ddb
Girlian Santos <girliansantos@si.fiponline.edu.br>
2022-05-22 09:10:53 -0300

    adicionando testes de unidade rails

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    validates :title, :description, :price, :image_url, presence: true
+    validates :price, numericality: {greater_than_or_equals_to: 0.01}
+    validates :title, uniqueness: true
+    validates :image_url, allow_blank: true, format:{
+        with: %r{\.(gif|jpg|png)\Z}i,
+        message: 'Somente são aceitos os formatos JPG, PNG ou GIF'
+    }
 end
